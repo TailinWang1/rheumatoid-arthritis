2b847ca8a82e7050d583ee7329414a34a94b0b6a
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-21 11:03:18 +0800

    Update Spatial analysis.R

--- Spatial analysis.R
+++ Spatial analysis.R
@@ -2,13 +2,13 @@ library(Seurat)
 library(ggplot2)
 library(patchwork)
 library(dplyr)
-
+#Please refer to the article for the GSMID used, or replace it with your own
 setwd("/Users/")
 sample_info <- list(
-  "WT" = "GSM5841550",
-  "Early" = "GSM5841551",
-  "Adv1" = "GSM5841552",
-  "Adv2" = "GSM5841553"
+  "WT" = "GSMID",
+  "Early" = "GSMID",
+  "Adv1" = "GSMID",
+  "Adv2" = "GSMID"
 )
 
 seurat_list <- list()
