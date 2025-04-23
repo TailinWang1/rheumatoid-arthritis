ab50a22a4da2d4a86c2bcdc8968f2e85ad196a5e
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-04-23 20:15:55 +0800

    Update Two-sample MR analysis.R

--- Two-sample MR analysis.R
+++ Two-sample MR analysis.R
@@ -18,20 +18,3 @@ pleio1<- mr_pleiotropy_test(dat1)
 OR1<-generate_odds_ratios(res1)
 het1 <- mr_heterogeneity(dat1)
 
-IL2RA<- extract_instruments(outcomes = "ebi-a-GCST90001935",p1=5e-06)
-outcome_dat2 <- extract_outcome_data(snps = IL2RA$SNP, outcomes = 'prot-c-5356_2_3')
-dat2<- harmonise_data(exposure_dat =IL2RA,  outcome_dat = outcome_dat2)
-res2<- mr(dat2)
-pleio2<- mr_pleiotropy_test(dat2)
-OR2<-generate_odds_ratios(res2)
-het2 <- mr_heterogeneity(dat2)
-
-MIF <- extract_instruments(outcomes = "prot-c-5356_2_3",p1=5e-06)
-outcome_dat3 <- extract_outcome_data(snps = MIF$SNP, outcomes = 'finn-b-RHEUMA_SEROPOS_OTH')
-dat3<- harmonise_data(exposure_dat =MIF,  outcome_dat = outcome_dat3)
-res3<- mr(dat3)
-pleio3<- mr_pleiotropy_test(dat3)
-OR3<-generate_odds_ratios(res3)
-het3 <- mr_heterogeneity(dat3)
-
-
