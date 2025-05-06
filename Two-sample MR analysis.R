be4b2d6123161b493be352722bc053192bfa362f
TailinWang1 <160468115+TailinWang1@users.noreply.github.com>
2025-05-06 21:10:25 +0800

    Update Two-sample MR analysis.R

--- Two-sample MR analysis.R
+++ Two-sample MR analysis.R
@@ -4,15 +4,15 @@ library(readxl)
 library(dplyr)
 library(ieugwasr)
 Sys.setenv(OPENGWAS_JWT="")
-ROMO1 <- extract_instruments(
-  outcomes = "eqtl-a-ENSG00000125995", 
+R <- extract_instruments(
+  outcomes = "eqtl-a-", 
   p1 = 5e-06, 
   clump = TRUE, 
   r2 = 0.001,
   kb=100)
 
-outcome_dat1 <- extract_outcome_data(snps = ROMO1$SNP, outcomes = 'ebi-a-GCST90001935')
-dat1<- harmonise_data(exposure_dat =ROMO1,  outcome_dat = outcome_dat1)
+outcome_dat1 <- extract_outcome_data(snps = R$SNP, outcomes = 'ebi-a-')
+dat1<- harmonise_data(exposure_dat =R,  outcome_dat = outcome_dat1)
 res1<- mr(dat1)
 pleio1<- mr_pleiotropy_test(dat1)
 OR1<-generate_odds_ratios(res1)
